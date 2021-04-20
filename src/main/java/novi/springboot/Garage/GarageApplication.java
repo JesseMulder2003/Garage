360ec8bb4f80e321e7c6ee015c7646027e17d9b8
Jesse <jesse200370@gmail.com>
2021-04-20 13:26:53 +0200

    Grage project NOVI

---
+++ src/main/java/novi/springboot/Garage/GarageApplication.java
@@ -0,0 +1,13 @@
+package novi.springboot.Garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageApplication.class, args);
+	}
+
+}
