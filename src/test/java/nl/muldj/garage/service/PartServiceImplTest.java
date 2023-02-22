6ecc8c0d660c1e71d5db6a6ac89dce59eb6bea32
Jesse Mulder <jesse200370@gmail.com>
2023-02-22 18:38:19 +0100

    toevoeging van mocking

--- src/test/java/nl/muldj/garage/service/PartServiceImplTest.java
+++ src/test/java/nl/muldj/garage/service/PartServiceImplTest.java
@@ -2,14 +2,16 @@ package nl.muldj.garage.service;
 
 import nl.muldj.garage.repository.PartRepository;
 import nl.muldj.garage.model.Part;
-import nl.muldj.garage.service.PartServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.any;
 
 @SpringBootTest
 public class PartServiceImplTest {
@@ -17,7 +19,7 @@ public class PartServiceImplTest {
     @Autowired
     private PartServiceImpl partService;
 
-    @Autowired
+    @MockBean
     private PartRepository partRepository;
 
 
@@ -26,13 +28,18 @@ public class PartServiceImplTest {
         //given
         Part part1 = new Part("ruitenwisser", "15", "5");
         Part part2 = new Part("supercharger", "650", "120");
-        int oldListSize = partRepository.findAll().size();
-        partRepository.saveAll(List.of(part1, part2));
+        int oldListSize = 0;
+//                partRepository.findAll().size();
+//        partRepository.saveAll(List.of(part1, part2));
+
+        Mockito
+                .when(partRepository.findAll())
+                .thenReturn(List.of(part1, part2));
 
         //when
         List<Part> expectedPart = partService.getAllParts();
 
         //then
-        assertThat(expectedPart.size()).isEqualTo(oldListSize + 2);
+        assertThat(expectedPart.size()).isEqualTo(2);
     }
 }
